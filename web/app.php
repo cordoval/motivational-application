022d1b3837bdaf22f3149c6029f67b5052059a7d
Loïc Chardonnet <loic.chardonnet@sensiolabs.com>
2013-08-16 21:38:48 +0200

    [UniqueFrontController] Merged app and app_dev with stack

--- web/app.php
+++ web/app.php
@@ -1,25 +1,30 @@
 <?php
 
-use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 
-// Use APC for autoloading to improve performance.
-// Change 'sf2' to a unique prefix in order to prevent cache key conflicts
-// with other applications also using APC.
-/*
-$loader = new ApcClassLoader('sf2', $loader);
-$loader->register(true);
-*/
-
 require_once __DIR__.'/../app/AppKernel.php';
-//require_once __DIR__.'/../app/AppCache.php';
 
-$kernel = new AppKernel('prod', false);
-$kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
+$app = new AppKernel('prod', false);
+$app->loadClassCache();
+
+// Prevent debug mode to be available on a production server.
+if (!isset($_SERVER['HTTP_CLIENT_IP'])
+    && !isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+    && in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1'))
+) {
+    Debug::enable();
+    $debugApplication = new AppKernel('dev', true);
+
+    $stack = new Stack\Builder();
+    $stack->push('Stack\UrlMap', array('/debug' => $debugApplication));
+
+    $app = $stack->resolve($app);
+}
+
 $request = Request::createFromGlobals();
-$response = $kernel->handle($request);
+$response = $app->handle($request);
 $response->send();
-$kernel->terminate($request, $response);
+$app->terminate($request, $response);
